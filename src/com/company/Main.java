866fc615a6d2ff9a6409c098d0c81d06f57e9419
NatnaelTsige <70252612+NatnaelTsige@users.noreply.github.com>
2020-08-26 12:04:31 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        newl();
+    }
+
+    public static void newl(){
+        System.out.println("fuck you");
+    }
+
+    public static int m = 9;
+}
